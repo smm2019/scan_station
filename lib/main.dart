26be0a8d743897c006d472f1ebbd02dba1d50cee
smm2019 <2474151346@qq.com>
2026-09-20 17:45:58 +0800

    fix:适配Isar3.1 filter API，增加CI Isar代码生成步骤，添加fileprovider文件路径配置，开启http明文访问

--- lib/main.dart
+++ lib/main.dart
@@ -44,8 +44,8 @@ $publicKeyBase64
   ASN1Parser asn1Parser = ASN1Parser(derBytes);
   ASN1Sequence topSeq = asn1Parser.nextObject() as ASN1Sequence;
   ASN1Sequence pubKeySeq = topSeq.elements[1] as ASN1Sequence;
-BigInt modulus = (pubKeySeq.elements[0] as ASN1Integer).integer;
-BigInt exponent = (pubKeySeq.elements[1] as ASN1Integer).integer;
+BigInt modulus = (pubKeySeq.elements[0] as ASN1Integer).value;
+BigInt exponent = (pubKeySeq.elements[1] as ASN1Integer).value;
   pc.RSAPublicKey pubKey = pc.RSAPublicKey(modulus, exponent);
 
   final cipher = pc.AsymmetricBlockCipher('RSA/PKCS1');
